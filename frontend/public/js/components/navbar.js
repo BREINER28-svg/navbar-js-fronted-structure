c2c16518b12c3cc4ce7c4fb977c3f9e03c50703e
Breiner Morales <breineradolfom@gmail.com>
2025-07-07 19:04:18 -0500

    feat: Se agrego el hero, se mejoraron los estilos del navbar

--- frontend/public/js/components/navbar.js
+++ frontend/public/js/components/navbar.js
@@ -1,3 +1,4 @@
+
  document.addEventListener("DOMContentLoaded", function(){
     const navbarElement = document.querySelector(".navbar-container");
 
@@ -6,6 +7,25 @@
         .then(response => response.text())
         .then(data => {
             navbarElement.innerHTML = data;
+            //===Logica para resaltar el enlace que esta activo en el navbar
+
+            //Obtener la ruta actual
+            //Si no hay un archivo especifico se asume que es el index 
+            const currentPage = window.location.pathname.split("/").pop() || "index.html";
+
+            //Selecciona todos los enlaces del nabvar que usaran la clase personalizada 
+            const navLinks = navbarElement.querySelectorAll(".navbar__link");
+
+            //Recorre cada enlace del nabvar 
+            navLinks.forEach(link => {
+
+                //verifica si el href del enlace incluye el nombre de la pagina actual
+                if(link.getAttribute("href").includes(currentPage)){
+                    //Si es la pagina actual se le asigna la clase 'active' para destacarla visualmente
+                    link.classList.add ("active");
+                }
+                
+            });
     })
     .catch(error => console.error("Error cargando el navbar", error));
 
